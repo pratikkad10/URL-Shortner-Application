8eab9dc12624a8aeb67e5df01c1343199ca0cb11
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-06-07 11:29:47 +0530

    feat: mounted the ensure auth middleware to the shorten url service

--- server/routes/url.routes.js
+++ server/routes/url.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { shortenController } from "../controllers/url.controller.js";
+import { ensureAuthMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/shorten", shortenController);
+router.post("/shorten", ensureAuthMiddleware, shortenController);
 
 export default router;
