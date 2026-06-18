1eda9b68cd89923b4744a28b75fdeadcf7724b2e
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-06-18 11:00:34 +0530

    feat: added fileds such as verification token and its expiry similarly for password verification

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,4 @@
-import { pgTable, varchar, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, varchar, boolean, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
@@ -6,6 +6,10 @@ export const usersTable = pgTable("users", {
   lastName: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: text().notNull(),
+  isVerified: boolean().default(false),
+  verificationToken: varchar({ length: 255 }),
+  resetPasswordOtp: varchar({ length: 6 }),
+  resetPasswordOtpExpiry: timestamp(),
   createdAt: timestamp().defaultNow(),
   updatedAt: timestamp().$onUpdate(() => new Date()),
 });
