e2d92e23548755368bdbd52f51943dea23127543
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-06-18 10:59:39 +0530

    feat: mounted the user routes with appropriate controllers

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,28 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    getCurrentUser,
+    resetPassword,
+    verifyEmail,
+    forgotPassword,
+    resetPasswordWithOtp
+} from "../controllers/user.controller.js";
+import { authMiddleware, ensureAuthMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// Public routes
 router.post("/register", registerUser);
-
 router.post("/login", loginUser);
+router.get("/verify/:token", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password-with-otp", resetPasswordWithOtp);
+
+// Protected routes (require user to be logged in)
+router.post("/logout", authMiddleware, ensureAuthMiddleware, logoutUser);
+router.get("/me", authMiddleware, ensureAuthMiddleware, getCurrentUser);
+router.post("/reset-password", authMiddleware, ensureAuthMiddleware, resetPassword);
 
 export default router;
